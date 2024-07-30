6316406d50f6dc5eabf09146045340464e0e098b
GideonBature <infoaboutgideon@gmail.com>
2024-07-30 14:55:43 +0100

    create wallet project

--- wallet/src/index.ts
+++ wallet/src/index.ts
@@ -1,10 +1,19 @@
 import { createApp } from "@deroll/app";
 import { createWallet } from "@deroll/wallet";
+import { hexToString, stringToHex } from "viem";
+
 
 const app = createApp({ url: process.env.ROLLUP_HTTP_SERVER_URL || "http://127.0.0.1:5004" });
 
 const wallet = createWallet();
 app.addAdvanceHandler(wallet.handler);
+
+app.addInspectHandler( async ({payload}) => {
+  const url = hexToString(payload).split("/");
+  console.log("Inspect calls: ", url);
+  const eth_balance = wallet.etherBalanceOf(<string>url[1]);
+  await app.createReport({ payload: stringToHex(String(eth_balance)) });
+})
 
 app.start().catch( (e) => {
   console.error(e);
