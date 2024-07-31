82ac9d1c3461cbcc0943eba4f78f46b726c49585
GideonBature <infoaboutgideon@gmail.com>
2024-07-31 16:56:54 +0100

    create wallet project

--- wallet/src/index.ts
+++ wallet/src/index.ts
@@ -1,12 +1,32 @@
 import { createApp } from "@deroll/app";
 import { createWallet } from "@deroll/wallet";
-import { hexToString, stringToHex } from "viem";
+import { getAddress, hexToString, stringToHex } from "viem";
 
 
 const app = createApp({ url: process.env.ROLLUP_HTTP_SERVER_URL || "http://127.0.0.1:5004" });
 
 const wallet = createWallet();
 app.addAdvanceHandler(wallet.handler);
+
+app.addAdvanceHandler( async ({metadata, payload}) => {
+  console.log("Input: ", metadata, payload);
+  const sender = getAddress(metadata.msg_sender);
+  try {
+    const jsonpayload = JSON.parse(hexToString(payload));
+    if (jsonpayload.method === "eth_withdraw") {
+      console.log("Withdrawing ether")
+      const amountToWithdraw: bigint = BigInt(jsonpayload.amount);
+      const voucher = wallet.withdrawEther(sender, amountToWithdraw);
+      await app.createVoucher(voucher);
+      return "accept"
+  } 
+  
+} catch (e) {
+    app.createReport({ payload: stringToHex(String(e)) });
+    return "reject"
+  }
+    return "accept"
+})
 
 app.addInspectHandler( async ({payload}) => {
   const url = hexToString(payload).split("/");
